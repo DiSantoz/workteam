7f6d3afe69a3c7c438c950d5c8db0b4a0887d66d
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-05 15:37:14 -0400

    create seed db for department and role

--- db/schema.sql
+++ db/schema.sql
@@ -8,10 +8,10 @@ CREATE TABLE department (
   name VARCHAR(30)
 );
 
-CREATE TABLE role(
+CREATE TABLE role (
   id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
-  salary DECIMAL(10,2),
+  salary DECIMAL(10,2) NOT NULL,
   department_id INTEGER
 );
 
---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Legal'),
+    ('Finance')
+
+INSERT INTO role (title, salary)
+VALUES
+    ("Salesperson", 30000.00),
+    ("Software Engineer", 60000.00),
+    ("Lawyer", 80000.00),
+    ("Accountant", 40000.00)
+
+
+    
