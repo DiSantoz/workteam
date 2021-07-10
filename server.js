1c6151e1b4fcec56fcbaf1a5643474d37b1ba231
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-07-09 21:51:34 -0400

    view all roles and departments

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require('console.table');
+require('console.table');
 
 // dotenv npm package to hide username and password
 require('dotenv').config();
@@ -16,31 +16,59 @@ const db = mysql.createConnection(
     console.log('Connected to company database.')
 );
 
-// view all employees
+// Ask user what they would like to do
 function team() {
     inquirer.prompt([
         {
             type: 'list',
             name: 'choice',
             message: 'What would you like to do?',
-            choices: ["View all employees", "Add employee", "Remove employee", "Update Employee role", "Update Employee manager"]
+            choices: ["View all employees", "View all departments", "View all roles", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
         }
     ]).then(function (answer) {
-        console.log(answer)
-
         if (answer.choice === 'View all employees') {
-            db.query(`SELECT * FROM role`, function (err, results) {
-                if (err) {
-                    console.log(err);
-                  } else {
-                    console.table(results);
-                  }
-                  
-            });
-        };
+            allEmployees();
+        } else if (answer.choice === 'View all departments') {
+            allDepartments();
+        } else if (answer.choice === 'View all roles')
+            allRoles();
     });
 };
 
+// view all employees
+function allEmployees() {
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, employee.manager_id 
+            FROM ((role
+            INNER JOIN department ON role.department_id = department.id)
+            INNER JOIN employee ON role.id = employee.role_id)`, function (err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+        }
+    });
+}
 
+// view all departments
+function allDepartments() {
+    db.query(`SELECT * FROM department`, function (err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+        }
+    });
+}
+
+// view all roles
+function allRoles() {
+    db.query(`SELECT * FROM role`, function (err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.table(results);
+        }
+    });
+}
 
 team();
